2a892136733397eb87388d33c295e2c275a68020
Emma Imprenti <diego.imprenti@issgreppi.it>
2023-12-02 11:12:16 +0100

    v0.1.3 prova Javascript

---
+++ accomodation.js
@@ -0,0 +1,6 @@
+function funzione1()
+{
+    document.getElementById("Secret").textContent("Secrets Room")
+    document.getElementById("3").textContent("La nostra secrets room e la stanza per le persone che vogliono divertirsi durante la loro vacanza scoprendo tutti i segreti della stanza");
+}
+
